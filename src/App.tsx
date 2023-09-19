a4861ea36727f9bef43a2f2331bc1556846c525f
ManarJemaa <145460217+ManarJemaa@users.noreply.github.com>
2023-09-19 16:47:30 +0200

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import { fetchTracks } from './lib/fetchTracks.ts';
+import { useQuery } from '@tanstack/react-query';
+import { SavedTrack, SpotifyType } from 'spotify-types';
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  };
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+  const AlbumCover = ({ track }) => {
+    const src = track;
+    return <img src={src} style={{ width: 400, height: 400 }} />;
+  };
+
+  //const trackUrls = tracks.map(track => track.track.preview_url);
+  console.log(tracks[0]);
+  if (tracks === undefined) {
+    return <div>Loading</div>;
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +32,13 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <AlbumCover track={tracks[0]?.track.album.images[0].url} />
+        <p>Hola</p>
+        <p>{trackIndex}</p>
       </div>
       <div className="App-buttons"></div>
+      <audio src={tracks[0]?.track.preview_url} autoPlay controls />
+      <button onClick={goToNextTrack}>Next track</button>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,18 @@
-const apiToken: string = '';
+import { SavedTrack, SpotifyType } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken =
+  'BQAublQN-YyZqbwYOUAg2X1tDydVej79bhuA1CYQE7xdNHpK-kcg4X_dOTo2ISPLe-CCDsaaUYXfgMHe194MWi1IC_XD8vIJpwrjBdWzLYIfZ6ARrv6q2yS27BDrf8wnrctzDXKbqVDCNGDqTALZxPy0kjvI-ALW0cqPKs9l1I5Vs-rTRD2yohTqMSMJzw1qMLlLjqAVWrXSNXOH7P8WCj9Nj7pAm9FDuw';
+
+export const fetchTracks = async (): Promise<SavedTrack> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack };
+  return data.items;
 };
